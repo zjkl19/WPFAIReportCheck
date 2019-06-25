6d03367eaac015f51d13f63455f61c4cd596a4a3
迪南 林 <eamdfan@hotmail.com>
2019-06-25 20:40:08 +0800

    add icon

--- WPFAIReportCheck/MainWindow.xaml.cs
+++ WPFAIReportCheck/MainWindow.xaml.cs
@@ -49,41 +49,7 @@ namespace WPFAIReportCheck
 
                 throw;
             }
-            //string fileName = "glz.doc";
 
-            //var doc = new Document(fileName);
-            //var ai = new AsposeAIReportCheck(doc);
-
-            //获取word文档中的第一个表格
-            //var table0 = doc.GetChildNodes(NodeType.Table, true)[1] as Table;
-
-            //var table0 = ai.GetOverViewTable();
-            //var cell = table0.Rows[4].Cells[1];
-            //string cbfbm = cell.GetText();
-
-            //string[] splitArray = cbfbm.Split('\r');
-            ////用GetText()的方法来获取cell中的值
-
-
-            //foreach (var s in splitArray)
-            //{
-            //    s.Replace("\a", "");
-            //    s.Replace("\r", "");
-            //    Console.WriteLine(s);
-            //    Levenshtein(@"《城市桥梁设计规范》（CJJ 11-2011）", @s);
-            //};
-
-
-
-            //if(ai.reportError!=null)
-            //{
-            //    foreach(var e in ai.reportError)
-            //    {
-            //        Console.WriteLine(e.No);
-            //        Console.WriteLine(e.Name);
-            //    }
-
-            //}
             //Document doc = new Document();
 
             //Paragraph para1 = new Paragraph(doc);
