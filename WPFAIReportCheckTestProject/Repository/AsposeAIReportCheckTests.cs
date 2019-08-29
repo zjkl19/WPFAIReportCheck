0e3efea04464e0c1cbc241679048c950599d2b4b
zjkl19 <eamdfan@126.com>
2019-08-29 17:30:21 +0800

    Depart FindSequenceNumberErrorTests.cs

--- WPFAIReportCheckTestProject/Repository/AsposeAIReportCheckTests.cs
+++ WPFAIReportCheckTestProject/Repository/AsposeAIReportCheckTests.cs
@@ -111,39 +111,6 @@ namespace WPFAIReportCheckTestProject.Repository
             Assert.True(docComment.GetText().IndexOf("应为《城市桥梁设计规范》（CJJ 11-2011）") >= 0);
         }
         #endregion
-
-        #region FindSequenceNumberError
-        [Fact]
-        public void FindSequenceNumberError()
-        {
-            //Arrange
-            var fileName = @"..\..\..\TestFiles\FindSequenceNumberError.doc";
-            var log = _fixture.log;
-            string xml = @"<?xml version=""1.0"" encoding=""utf - 8"" ?>
-                            <configuration>
-                              <FindStrainOrDispError row1=""0"" col1=""0"" row2 =""1"" col2 =""1""  charactorString =""测点号"" >
-                                <Strain charactorString = ""总应变"" />
-                                <Disp charactorString = ""总变形"" />
-                             </FindStrainOrDispError >
-                           </configuration >";
-            var config = XDocument.Parse(xml);
-
-            var ai = new AsposeAIReportCheck(fileName, log.Object, config);
-            //Act
-            ai.FindSequenceNumberError();
-
-            using (MemoryStream dstStream = new MemoryStream())
-            {
-                ai._doc.Save(dstStream, SaveFormat.Doc);
-            }
-
-            Comment docComment = (Comment)ai._doc.GetChild(NodeType.Comment, 0, true);
-            NodeCollection allComments = ai._doc.GetChildNodes(NodeType.Comment, true);
-
-            //Assert
-            Assert.Single(allComments);
-            Assert.True(docComment.GetText().IndexOf("序号应连贯，从小到大") >= 0);
-        }
-        #endregion
+      
     }
 }
---
+++ WPFAIReportCheckTestProject/Repository/FindSequenceNumberErrorTests.cs
@@ -0,0 +1,50 @@
+﻿using Xunit;
+using WPFAIReportCheck.Repository;
+using WPFAIReportCheck.Models;
+//using Aspose.Words;
+using System.IO;
+using Moq;
+using System;
+using System.Xml.Linq;
+using Aspose.Words;
+
+namespace WPFAIReportCheckTestProject.Repository
+{
+    public partial class AsposeAIReportCheckTests : IClassFixture<AsposeAIReportCheckTestsFixture>
+    {
+
+        #region FindSequenceNumberError
+        [Fact]
+        public void FindSequenceNumberError()
+        {
+            //Arrange
+            var fileName = @"..\..\..\TestFiles\FindSequenceNumberError.doc";
+            var log = _fixture.log;
+            string xml = @"<?xml version=""1.0"" encoding=""utf - 8"" ?>
+                            <configuration>
+                              <FindStrainOrDispError row1=""0"" col1=""0"" row2 =""1"" col2 =""1""  charactorString =""测点号"" >
+                                <Strain charactorString = ""总应变"" />
+                                <Disp charactorString = ""总变形"" />
+                             </FindStrainOrDispError >
+                           </configuration >";
+            var config = XDocument.Parse(xml);
+
+            var ai = new AsposeAIReportCheck(fileName, log.Object, config);
+            //Act
+            ai.FindSequenceNumberError();
+
+            using (MemoryStream dstStream = new MemoryStream())
+            {
+                ai._doc.Save(dstStream, SaveFormat.Doc);
+            }
+
+            Comment docComment = (Comment)ai._doc.GetChild(NodeType.Comment, 0, true);
+            NodeCollection allComments = ai._doc.GetChildNodes(NodeType.Comment, true);
+
+            //Assert
+            Assert.Single(allComments);
+            Assert.True(docComment.GetText().IndexOf("序号应连贯，从小到大") >= 0);
+        }
+        #endregion
+    }
+}
