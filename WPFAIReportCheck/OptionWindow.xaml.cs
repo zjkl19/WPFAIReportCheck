84e8f3eb6519395a7a32c4d251421f5d5be7a9d6
迪南 林 <eamdfan@hotmail.com>
2019-08-30 20:36:24 +0800

    Add the Save Option feature

--- WPFAIReportCheck/OptionWindow.xaml.cs
+++ WPFAIReportCheck/OptionWindow.xaml.cs
@@ -20,28 +20,30 @@ namespace WPFAIReportCheck
     /// </summary>
     public partial class OptionWindow : Window
     {
+        public List<CheckBox> OptionCheckBoxList;
         public OptionWindow()
         {
             InitializeComponent();
             var config = XDocument.Load(@"Option.config");
-            //var t = Convert.ToInt32(config.Element("configuration").Element("FindUnitError").Value);
-            //if(t==1)
-            //{
-            //    FindUnitErrorCheckBox.IsChecked= true;
-            //}
-
-            foreach (var e in config.Elements("configuration").Descendants())
+            try
             {
-                try
+                OptionCheckBoxList = new List<CheckBox>();
+                foreach (var e in config.Elements("configuration").Descendants())
                 {
-                    CheckBox cb = new CheckBox();
-                    cb.Name= e.Attribute("Name").Value.ToString();
-                    cb.Content = e.Attribute("Content").Value.ToString();
-                    if(cb.Content.ToString().Length<8)
+                    CheckBox cb = new CheckBox
+                    {
+                        Name = e.Attribute("Name").Value.ToString(),
+                        Content = e.Attribute("Content").Value.ToString(),
+                        Height = 30,
+                        VerticalAlignment = VerticalAlignment.Center,
+                        VerticalContentAlignment = VerticalAlignment.Center
+                    };
+
+                    if (cb.Content.ToString().Length < 10)
                     {
                         cb.Width = 130;
                     }
-                    else if(cb.Content.ToString().Length > 8 && cb.Content.ToString().Length <13)
+                    else if (cb.Content.ToString().Length > 10 && cb.Content.ToString().Length < 15)
                     {
                         cb.Width = 180;
                     }
@@ -49,47 +51,47 @@ namespace WPFAIReportCheck
                     {
                         cb.Width = 200;
                     }
-                    cb.Height = 30;
-                    cb.VerticalAlignment= VerticalAlignment.Center;
-                    cb.VerticalContentAlignment = VerticalAlignment.Center;
+
+                    if(Convert.ToInt32(e.Value)==1)
+                    {
+                        cb.IsChecked = true;
+                    }
                     //cb.Tag = i;
                     //Thickness thickness = new Thickness(5);
                     //cb.VerticalContentAlignment = VerticalAlignment.Center;
                     //cb.Margin = thickness;
                     //cb.Click += cb_Click;
                     //cb.DataContext = new Test() { ID = i, Name = cb.Content.ToString() };
+
                     StackPanelContent.Children.Add(cb);
+
                     //dicSelPersonnal.Add(cb.Content.ToString(), false);
+                    OptionCheckBoxList.Add(cb);
                 }
-                catch (Exception ex)
-                {
-                    //TODO:数据格式不正确时的异常处理
-                    throw ex;
-                }
+
             }
-
-            //for (int i = 1; i <= 15; i++)
-            //{
-
-            //    CheckBox cb = new CheckBox();
-            //    cb.Content = "名称" + i;
-            //    cb.Width = 80;
-            //    cb.Tag = i;
-            //    Thickness thickness = new Thickness(5);
-            //    cb.VerticalContentAlignment = VerticalAlignment.Center;
-            //    cb.Margin = thickness;
-            //    cb.Click += cb_Click;
-            //    cb.DataContext = new Test() { ID = i, Name = cb.Content.ToString() };
-            //    StackPanelContent.Children.Add(cb);
-            //    dicSelPersonnal.Add(cb.Content.ToString(), false);
-
-            //}
-
+            catch (Exception ex)
+            {
+                //TODO:数据格式不正确时的异常处理
+                throw ex;
+            }
         }
-        class Test
+
+        //TODO：单元测试
+        private void Save_Button_Click(object sender, RoutedEventArgs e)
         {
-            public int ID { get; set; }
-            public string Name { get; set; }
+            foreach(var cb in OptionCheckBoxList)
+            {
+                var config = XDocument.Load(@"Option.config");
+                var MethodUpdate = (from p in config.Element("configuration").Elements() where (p.Attribute("Name").Value) == cb.Name select p).FirstOrDefault();
+                if (MethodUpdate != null)
+                {
+                    MethodUpdate.Value = (cb.IsChecked ?? false) ? "1" : "0";
+                    config.Save(@"Option.config");
+                }
+
+            }
+            MessageBox.Show("保存设置成功！");
         }
     }
 }
