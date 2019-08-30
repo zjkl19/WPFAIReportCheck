af9ce46c4ffcd2cbea53fe50dfc156410c39e359
zjkl19 <eamdfan@126.com>
2019-08-30 18:18:38 +0800

    Upgrade Option UI

--- WPFAIReportCheck/OptionWindow.xaml.cs
+++ WPFAIReportCheck/OptionWindow.xaml.cs
@@ -24,12 +24,72 @@ namespace WPFAIReportCheck
         {
             InitializeComponent();
             var config = XDocument.Load(@"Option.config");
-            var t = Convert.ToInt32(config.Element("configuration").Element("FindUnitError").Value);
-            if(t==1)
+            //var t = Convert.ToInt32(config.Element("configuration").Element("FindUnitError").Value);
+            //if(t==1)
+            //{
+            //    FindUnitErrorCheckBox.IsChecked= true;
+            //}
+
+            foreach (var e in config.Elements("configuration").Descendants())
             {
-                FindUnitErrorCheckBox.IsChecked= true;
+                try
+                {
+                    CheckBox cb = new CheckBox();
+                    cb.Name= e.Attribute("Name").Value.ToString();
+                    cb.Content = e.Attribute("Content").Value.ToString();
+                    if(cb.Content.ToString().Length<8)
+                    {
+                        cb.Width = 130;
+                    }
+                    else if(cb.Content.ToString().Length > 8 && cb.Content.ToString().Length <13)
+                    {
+                        cb.Width = 180;
+                    }
+                    else    //字符串>=13的情况待定
+                    {
+                        cb.Width = 200;
+                    }
+                    cb.Height = 30;
+                    cb.VerticalAlignment= VerticalAlignment.Center;
+                    cb.VerticalContentAlignment = VerticalAlignment.Center;
+                    //cb.Tag = i;
+                    //Thickness thickness = new Thickness(5);
+                    //cb.VerticalContentAlignment = VerticalAlignment.Center;
+                    //cb.Margin = thickness;
+                    //cb.Click += cb_Click;
+                    //cb.DataContext = new Test() { ID = i, Name = cb.Content.ToString() };
+                    StackPanelContent.Children.Add(cb);
+                    //dicSelPersonnal.Add(cb.Content.ToString(), false);
+                }
+                catch (Exception ex)
+                {
+                    //TODO:数据格式不正确时的异常处理
+                    throw ex;
+                }
             }
 
+            //for (int i = 1; i <= 15; i++)
+            //{
+
+            //    CheckBox cb = new CheckBox();
+            //    cb.Content = "名称" + i;
+            //    cb.Width = 80;
+            //    cb.Tag = i;
+            //    Thickness thickness = new Thickness(5);
+            //    cb.VerticalContentAlignment = VerticalAlignment.Center;
+            //    cb.Margin = thickness;
+            //    cb.Click += cb_Click;
+            //    cb.DataContext = new Test() { ID = i, Name = cb.Content.ToString() };
+            //    StackPanelContent.Children.Add(cb);
+            //    dicSelPersonnal.Add(cb.Content.ToString(), false);
+
+            //}
+
+        }
+        class Test
+        {
+            public int ID { get; set; }
+            public string Name { get; set; }
         }
     }
 }
