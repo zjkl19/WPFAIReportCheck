c97cbf81d669ca91904ed79d342ad837d22bc9c0
zjkl19 <eamdfan@126.com>
2019-08-30 16:26:21 +0800

    Delete extra comments and code

--- WPFAIReportCheck/Repository/CheckReport.cs
+++ WPFAIReportCheck/Repository/CheckReport.cs
@@ -13,15 +13,9 @@ namespace WPFAIReportCheck.Repository
 
     public partial class AsposeAIReportCheck : IAIReportCheck
     {
-        public delegate void CheckFunctionHandler();
-        public event CheckFunctionHandler UpgradeProgressBar;
-
-        private delegate void MyDelegate();
+        private delegate void MethodDelegate();
         public void CheckReport()
         {
-
-            var fs = new List<MyDelegate>();
-            fs.Add(this.FindDescriptionError);
 
             //举例：
             //SelectListInt =[1,0]
@@ -30,16 +24,14 @@ namespace WPFAIReportCheck.Repository
             var controlClass = new
             {
                 SelectListInt = new List<int>(),
-                SelectListFunctionName = new List<MyDelegate> {
+                SelectListFunctionName = new List<MethodDelegate> {
                     _FindUnitError,_FindNotExplainComponentNo,_FindSpecificationsError,
                     FindSequenceNumberError,FindStrainOrDispError,FindDescriptionError,
                     FindOtherBridgesWarnning,
                 },
             };
-
-            
+    
             var config = XDocument.Load(@"Option.config");
-            var op = Convert.ToInt32(config.Element("configuration").Element("FindUnitError").Value);
 
             //Bug:修改配置文件后，读取结果不变
             foreach (var e in config.Elements("configuration").Descendants())
@@ -55,31 +47,11 @@ namespace WPFAIReportCheck.Repository
                 }
             }
 
-            //foreach (XElement e in config.Elements("configuration"))
-            //{
-            //    try
-            //    {
-            //        controlClass.SelectListInt.Add(Convert.ToInt32(e.Value));
-            //    }
-            //    catch (Exception)
-            //    {
-            //        //TODO:增加读不出数据的异常处理
-            //        controlClass.SelectListInt.Add(0);
-            //    }   
-            //}
-
             var w = new ProgressBarWindow();
             w.Top = 0.4 * (App.ScreenHeight - w.Height);
             w.Left = 0.4 * (App.ScreenWidth - w.Width);
 
-            var progressSleepTime = 50;
-
-            //CheckFunction cf;
-            //cf = _FindUnitError;
-            //cf += _FindNotExplainComponentNo;
-            //cf += _FindSpecificationsError;
-            UpgradeProgressBar += _FindUnitError;
-
+            var progressSleepTime = 50;    //进度条停顿时间
 
             var thread = new Thread(new ThreadStart(() =>
             {
@@ -95,32 +67,10 @@ namespace WPFAIReportCheck.Repository
                         Thread.Sleep(progressSleepTime);
                     }
                     
-                }
-                //_FindUnitError();
-                //w.progressBar.Dispatcher.BeginInvoke((ThreadStart)delegate { w.progressBar.Value = 10; });
-                //Thread.Sleep(progressSleepTime);
-                //_FindNotExplainComponentNo();
-                //w.progressBar.Dispatcher.BeginInvoke((ThreadStart)delegate { w.progressBar.Value = 20; });
-                //Thread.Sleep(progressSleepTime);
-                //_FindSpecificationsError();
-                //w.progressBar.Dispatcher.BeginInvoke((ThreadStart)delegate { w.progressBar.Value = 30; });
-                //Thread.Sleep(progressSleepTime);
-                //FindSequenceNumberError();
-                //w.progressBar.Dispatcher.BeginInvoke((ThreadStart)delegate { w.progressBar.Value = 40; });
-                //Thread.Sleep(progressSleepTime);
-                //FindStrainOrDispError();
-                //w.progressBar.Dispatcher.BeginInvoke((ThreadStart)delegate { w.progressBar.Value = 50; });
-                //Thread.Sleep(progressSleepTime);
-                //FindDescriptionError();
-                //w.progressBar.Dispatcher.BeginInvoke((ThreadStart)delegate { w.progressBar.Value = 60; });
-                //Thread.Sleep(progressSleepTime);
-                //FindOtherBridgesWarnning();
-                //w.progressBar.Dispatcher.BeginInvoke((ThreadStart)delegate { w.progressBar.Value = 70; });
-                //Thread.Sleep(progressSleepTime);
-
+                }       
                 _GenerateResultReport();
                 _doc.Save("标出错误或警告的报告.doc");
-                Thread.Sleep(100);
+                Thread.Sleep(progressSleepTime);
                 MessageBox.Show("已成功校核");
                 w.progressBar.Dispatcher.BeginInvoke((ThreadStart)delegate { w.Close(); });
             }));
