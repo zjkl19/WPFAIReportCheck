b0983aa16cb13f969f1b8e3f4a766c39929bc95a
迪南 林 <eamdfan@hotmail.com>
2019-09-13 22:35:57 +0800

    add the FindTableTitleSequenceNumberError feature

--- WPFAIReportCheck/Repository/FindTableTitleSequenceNumberError.cs
+++ WPFAIReportCheck/Repository/FindTableTitleSequenceNumberError.cs
@@ -2,20 +2,22 @@
 using System.Collections.Generic;
 using System.Text.RegularExpressions;
 using Aspose.Words;
-using Aspose.Words.Replacing;
-using Aspose.Words.Tables;
+using System.Linq;
 using WPFAIReportCheck.IRepository;
 using WPFAIReportCheck.Models;
 
 namespace WPFAIReportCheck.Repository
 {
-    public class ProgressBarDataBinding1 
+    public class TableTitleClass
     {
-
+        /// <summary>
+        /// 表头标题，如：表3-2
+        /// </summary>
         public string TableTitle { get; set; }
-
+        /// <summary>
+        /// 插入批注使用的定位结点
+        /// </summary>
         public Node TableTitleNode { get; set; }
-
     }
 
     public partial class AsposeAIReportCheck : IAIReportCheck
@@ -36,19 +38,23 @@ namespace WPFAIReportCheck.Repository
         /// 3.4、若章节号不同于一张表格，则表序号==1
         /// </remarks>
         /// 算法主要参与人员：林迪南、许智星
-        /// TODO：考虑表格标题超过1行的情况
+        /// TODO：
+        /// 1、考虑表格标题超过1行的情况
+        /// 2、_doc.GetChildNodes(NodeType.Table, true)[i].PreviousSibling.Range.Text没有表格标题，但标题就在附近的情况
         public void FindTableTitleSequenceNumberError()
         {
             Match m;
-            var list = new List<string>();
+            List<TableTitleClass> list;
             var regex = new Regex(@"\b表[\s]{0,1}[1-9][0-9]?[0-9]?[\s]{0,1}-[\s]{0,1}[1-9][0-9]?[0-9]?");
             int previousChapterNum = 0; int previousTableSequenceNum = 0; int currChapterNum = 0; int currTableSequenceNum = 0;
             //var regex = new Regex(@"(?<=\(附[\s]{0,4}页\)\r目录\r)[\s\S]*?(?=\f)"); 
             var chapterNumRegex = new Regex(@"(?<=表)[1-9][0-9]?[0-9]?(?=\-)");
             var tableSequenceNumRegex = new Regex(@"(?<=\-)[1-9][0-9]?[0-9]?");
+
+            Comment comment;
             try
             {
-                list = new List<string>();
+                list = new List<TableTitleClass>();
                 for (int i = 0; i < _doc.GetChildNodes(NodeType.Table, true).Count; i++)
                 {
                     try
@@ -56,7 +62,10 @@ namespace WPFAIReportCheck.Repository
                         m = regex.Match(_doc.GetChildNodes(NodeType.Table, true)[i].PreviousSibling.Range.Text);
                         if (m.Success)
                         {
-                            list.Add(m.Value.ToString().Replace(" ", "")); ;
+                            list.Add(new TableTitleClass {
+                                TableTitle= m.Value.ToString().Replace(" ", ""),
+                                TableTitleNode= _doc.GetChildNodes(NodeType.Table, true)[i].PreviousSibling
+                            });
                         }
                     }
                     catch (Exception)
@@ -65,18 +74,27 @@ namespace WPFAIReportCheck.Repository
                     }
 
                 }
-                list.Sort();
+                list.OrderBy(x=>x.TableTitle);
+
+
                 for (int i = 0; i < list.Count; i++)
                 {
-                    currChapterNum = Convert.ToInt32(chapterNumRegex.Match(list[i]).Value);
-                    currTableSequenceNum = Convert.ToInt32(tableSequenceNumRegex.Match(list[i]).Value);
+                    currChapterNum = Convert.ToInt32(chapterNumRegex.Match(list[i].TableTitle).Value);
+                    currTableSequenceNum = Convert.ToInt32(tableSequenceNumRegex.Match(list[i].TableTitle).Value);
                     if (i == 0)
                     {
-                        previousChapterNum = Convert.ToInt32(chapterNumRegex.Match(list[i]).Value);
-                        previousTableSequenceNum = Convert.ToInt32(tableSequenceNumRegex.Match(list[i]).Value);
+                        previousChapterNum = Convert.ToInt32(chapterNumRegex.Match(list[i].TableTitle).Value);
+                        previousTableSequenceNum = Convert.ToInt32(tableSequenceNumRegex.Match(list[i].TableTitle).Value);
                         if (currTableSequenceNum != 1)
                         {
                             reportError.Add(new ReportError(ErrorNumber.Description, $"第1张表", $"第1张表格起始序号应为1", true));
+
+                            comment = new Comment(_doc, "AI", "AI校核", DateTime.Now);
+                            comment.Paragraphs.Add(new Paragraph(_doc));
+                            comment.FirstParagraph.Runs.Add(new Run(_doc, $"第1张表格起始序号应为1"));
+                            DocumentBuilder builder = new DocumentBuilder(_doc);
+                            builder.MoveTo(list[i].TableTitleNode as Paragraph);
+                            builder.CurrentParagraph.AppendChild(comment);
                         }
                     }
                     else
@@ -86,6 +104,13 @@ namespace WPFAIReportCheck.Repository
                             if(currTableSequenceNum- previousTableSequenceNum!=1)
                             {
                                 reportError.Add(new ReportError(ErrorNumber.Description, $"第{i+1}张表", $"第{i+1}张表格序号为{currChapterNum}-{currTableSequenceNum}，实际应为{currChapterNum}-{previousTableSequenceNum+1}", true));
+
+                                comment = new Comment(_doc, "AI", "AI校核", DateTime.Now);
+                                comment.Paragraphs.Add(new Paragraph(_doc));
+                                comment.FirstParagraph.Runs.Add(new Run(_doc, $"本表格序号应为{currChapterNum}-{previousTableSequenceNum + 1}"));
+                                DocumentBuilder builder = new DocumentBuilder(_doc);
+                                builder.MoveTo(list[i].TableTitleNode as Paragraph);
+                                builder.CurrentParagraph.AppendChild(comment);
                             }
                         }
                         else   //(currChapterNum>previousChapterNum)
@@ -93,6 +118,13 @@ namespace WPFAIReportCheck.Repository
                             if (currTableSequenceNum != 1)
                             {
                                 reportError.Add(new ReportError(ErrorNumber.Description, $"第{i+1}张表", $"第{i+1}张表格序号为{currChapterNum}-{currTableSequenceNum}，实际应为{currChapterNum}-1", true));
+
+                                comment = new Comment(_doc, "AI", "AI校核", DateTime.Now);
+                                comment.Paragraphs.Add(new Paragraph(_doc));
+                                comment.FirstParagraph.Runs.Add(new Run(_doc, $"本表格序号应为{currChapterNum}-1"));
+                                DocumentBuilder builder = new DocumentBuilder(_doc);
+                                builder.MoveTo(list[i].TableTitleNode as Paragraph);
+                                builder.CurrentParagraph.AppendChild(comment);
                             }
                         }
                     }
---
+++ WPFAIReportCheckTestProject/Repository/FindTableTitleSequenceNumberErrorTests.cs
@@ -0,0 +1,61 @@
+﻿using Xunit;
+using WPFAIReportCheck.Repository;
+using WPFAIReportCheck.Models;
+using Aspose.Words;
+using System.IO;
+using Moq;
+using System;
+using System.Xml.Linq;
+using NLog;
+
+namespace WPFAIReportCheckTestProject.Repository
+{
+    public partial class AsposeAIReportCheckTests : IClassFixture<AsposeAIReportCheckTestsFixture>
+    {
+        [Fact]
+        public void FindTableTitleSequenceNumberError_ReturnsCorrectCountOfReportError()
+        {
+            //Arrange
+            var fileName = @"..\..\..\TestFiles\FindTableTitleSequenceNumberError.doc";
+
+            var log = _fixture.log;
+            string xml = DataInitializer.AIReportCheckXml;
+            var config = XDocument.Parse(xml);
+
+            var ai = new AsposeAIReportCheck(fileName, log.Object, config);
+            //Act
+            ai.FindTableTitleSequenceNumberError();
+            //Assert
+            Assert.Equal(2,ai.reportError.Count);
+            Assert.Equal(ErrorNumber.Description, ai.reportError[0].No);
+        }
+
+        [Fact]
+        public void FindTableTitleSequenceNumberError_WritesCorrectCommentsInDoc()
+        {
+            //Arrange
+            var fileName = @"..\..\..\TestFiles\FindTableTitleSequenceNumberError.doc";
+
+            var log = _fixture.log;
+            string xml = DataInitializer.AIReportCheckXml;
+            var config = XDocument.Parse(xml);
+            var ai = new AsposeAIReportCheck(fileName, log.Object, config);
+            //Act
+            ai.FindTableTitleSequenceNumberError();
+
+            using (MemoryStream dstStream = new MemoryStream())
+            {
+                ai._doc.Save(dstStream, SaveFormat.Doc);
+            }
+
+            Comment docComment1 = (Comment)ai._doc.GetChild(NodeType.Comment, 0, true);
+            Comment docComment2 = (Comment)ai._doc.GetChild(NodeType.Comment, 1, true);
+            NodeCollection allComments = ai._doc.GetChildNodes(NodeType.Comment, true);
+
+            //Assert
+            Assert.Equal(2, allComments.Count);
+            Assert.Contains("本表格序号应为3-3", docComment1.GetText());
+            Assert.Contains("本表格序号应为3-5", docComment2.GetText());
+        }
+    }
+}
